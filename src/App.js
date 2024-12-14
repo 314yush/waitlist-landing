6f9ebc45ff6716c7c0823185c79c1a384eefb9cc
314yush <startrek2219@gmail.com>
2024-12-14 15:13:40 +0530

    TEST

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  const handleClick = () => {
+    window.location.href = "hhttps://app.deform.cc/form/83ae6a90-7d88-4932-981b-ce2f1d76dc20";
+  };
+
+  return (
+    <>
+      <nav className="nav">
+        <div><a href="/" className="logo">gohigher.life</a></div>
+        <div>
+          <a href="/about">about</a>
+          <a href="/follow">follow</a>
+        </div>
+      </nav>
+      <div className="container">
+        <h1>Earn ↑ for every steps</h1>
+        <button onClick={handleClick}>Join Waitlist</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
